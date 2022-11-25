cbb6768914805be8a104414ce9106afad1058a39
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-11-25 13:31:03 +0300

    Добавлен сервлет для главной страницы приложения

---
+++ src/main/java/com/yaroshevich/app/controller/MainServlet.java
@@ -0,0 +1,44 @@
+package com.yaroshevich.app.controller;
+
+import com.yaroshevich.app.dao.EmployeeDao;
+import com.yaroshevich.app.dto.EmployeeDto;
+import com.yaroshevich.app.service.EmployeeService;
+import com.yaroshevich.app.util.DBConnector;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "MainServlet", value = "/")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Connection connection = new DBConnector().getConnection();
+
+        EmployeeDao employeeDao = new EmployeeDao(connection);
+
+        EmployeeService employeeService = new EmployeeService(employeeDao);
+
+        List<EmployeeDto> employees = new ArrayList<>();
+
+        try {
+            employees = employeeService.getAllWithDetails();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        request.setAttribute("employees", employees);
+
+        request.getRequestDispatcher("/view/index.jsp").forward(request, response);
+
+    }
+
+}
