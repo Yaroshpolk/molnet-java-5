ffd0ee5b140085669f0ce3c413a5bc7f3338bfe1
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-11-25 12:19:45 +0300

    Добавил в employeeDAO и маппер метод для получения данных о работнике, включая вложенные объекты

--- src/main/java/com/yaroshevich/app/dao/EmployeeDao.java
+++ src/main/java/com/yaroshevich/app/dao/EmployeeDao.java
@@ -1,8 +1,8 @@
 package com.yaroshevich.app.dao;
 
+import com.yaroshevich.app.dto.EmployeeDto;
 import com.yaroshevich.app.mapper.EmployeeMapper;
 import com.yaroshevich.app.model.Employee;
-import com.yaroshevich.app.util.DBConnector;
 import com.yaroshevich.app.util.DBWorker;
 
 import java.sql.Connection;
@@ -11,13 +11,28 @@ import java.util.List;
 
 public class EmployeeDao implements Dao<Employee> {
 
-    private final Connection connection = new DBConnector().getConnection();
+    private final Connection connection;
+
+    public EmployeeDao(Connection connection) {
+        this.connection = connection;
+    }
 
     public List<Employee> getAll() throws SQLException {
         DBWorker dbWorker = new DBWorker(connection);
         EmployeeMapper mapper = new EmployeeMapper();
 
         return mapper.map(dbWorker.executeQuery("SELECT * FROM employees"));
+    }
+
+    public List<EmployeeDto> getAllWithDetails() throws SQLException {
+        DBWorker dbWorker = new DBWorker(connection);
+        EmployeeMapper mapper = new EmployeeMapper();
+
+        return mapper.mapWithDetails(dbWorker.executeQuery("SELECT employees.id as employee_id, first_name, last_name," +
+                " patronymic, age, ha.id as address_id, ha.address, ha.district, ha.region, s.id as shift_id, " +
+                "s.start_at, s.end_at FROM employees\n" +
+                "LEFT JOIN home_addresses ha on ha.id = employees.address_id\n" +
+                "LEFT JOIN shifts s on s.id = employees.shift_id"));
     }
 
 }
--- src/main/java/com/yaroshevich/app/mapper/EmployeeMapper.java
+++ src/main/java/com/yaroshevich/app/mapper/EmployeeMapper.java
@@ -1,6 +1,9 @@
 package com.yaroshevich.app.mapper;
 
+import com.yaroshevich.app.dto.EmployeeDto;
+import com.yaroshevich.app.model.Address;
 import com.yaroshevich.app.model.Employee;
+import com.yaroshevich.app.model.Shift;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,6 +25,36 @@ public class EmployeeMapper implements Mapper<Employee> {
             int shiftId = resultSet.getInt("shift_id");
 
             Employee employee = new Employee(id, firstName, lastName, patronymic, age, addressId, shiftId);
+            list.add(employee);
+        }
+
+        return list;
+    }
+
+    public List<EmployeeDto> mapWithDetails(ResultSet resultSet) throws SQLException {
+        List<EmployeeDto> list = new ArrayList<>();
+
+        while (resultSet.next()) {
+
+            int id = resultSet.getInt("employee_id");
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String patronymic = resultSet.getString("patronymic");
+            int age = resultSet.getInt("age");
+
+            int addressId = resultSet.getInt("address_id");
+            String address = resultSet.getString("address");
+            String district = resultSet.getString("district");
+            String region = resultSet.getString("region");
+
+            int shiftId = resultSet.getInt("shift_id");
+            String startAt = resultSet.getString("start_at");
+            String endAt = resultSet.getString("end_at");
+
+            Address addressObj = new Address(addressId, address, district, region);
+            Shift shiftObj = new Shift(shiftId, startAt, endAt);
+
+            EmployeeDto employee = new EmployeeDto(id, firstName, lastName, patronymic, age, addressObj, shiftObj);
             list.add(employee);
         }
 
