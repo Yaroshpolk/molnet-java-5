a6e7c5ab92555794a7736ea95176d71fe638fc6e
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-12-25 18:51:15 +0300

    Добавлены стили для некорректных полей в формах

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -29,23 +29,39 @@ const createErrorElem = (txt) => {
 }
 
 const clearFormErrors = (form) => {
+    let invalidFields = form.querySelectorAll(".form__input_invalid");
+
+    invalidFields.forEach(item => {
+        item.classList.remove("form__input_invalid");
+    })
+
     let errors = form.querySelectorAll(".form__error");
     errors.forEach(error => error.remove());
 }
 
 const checkInputValidity = (item) => {
+    let itemValidity = true;
+
     if (item.required && !item.value) {
         item.parentElement.append(createErrorElem("Это поле обязательно к заполнению"))
+        itemValidity = false;
     } else if (item.minLength > item.value.length && item.minLength !== -1) {
         item.parentElement.append(createErrorElem("Минимальная длинна текста " + item.minLength + " символа"))
+        itemValidity = false;
     } else if (item.maxLength < item.value.length && item.maxLength !== -1) {
         item.parentElement.append(createErrorElem("Максимальная длинна текста " + item.maxLength + " символа"))
+        itemValidity = false;
     }
 
     if (item.required && item.tagName === 'SELECT') {
         if (item.value === "0") {
             item.parentElement.append(createErrorElem("Это поле обязательно к заполнению"))
+            itemValidity = false;
         }
+    }
+
+    if (itemValidity === false && item.tagName !== 'SELECT') {
+        item.classList.add("form__input_invalid")
     }
 }
 
